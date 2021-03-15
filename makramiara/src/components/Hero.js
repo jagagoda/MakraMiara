7af60a2f5f5093d854bd65f030104f990d734c7f
Jagoda <76485036+jagagoda@users.noreply.github.com>
2021-03-15 20:50:59 +0100

    Update Hero.js

--- makramiara/src/components/Hero.js
+++ makramiara/src/components/Hero.js
@@ -1,55 +1,67 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Macrame from '../images/macrame.png';
 
 
 const HeroSection = styled.section`
   display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  align-items: center;
+ flex-direction: column;
   text-align: center;
   background-color: #F4F4F4;
   width: 100%;
-  height: 100vh;
-  min-height: 800px;
-  position: relative;
+  height: 100%;
+  min-height: 30rem;
+  @media screen and (min-width: 800px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    text-align: center;
+    background-color: #F4F4F4;
+    width: 100%;
+    height: 100vh;
+    min-height: 50rem;
+  }
   .image {
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-right: 50px;
+    @media screen and (min-width: 800x) {
+      margin-right: 3.1rem;
+    }
   }
   img {
-    width: 700px;
-    margin-top: 100px;
+    width: 18rem;
+    margin-top: 8rem;
     flex: 1;
-    margin-left: 300px;
+    @media screen and (min-width: 800px) {
+      width: 44rem;
+      margin-left: 19rem;
+    }
   }
 `;
 
 const HeroDescription = styled.div`
-  width: 50%;
+  width: 100%;
   background-color: transparent;
   color: #304c28;
   z-index: 1;
+  @media screen and (min-width: 800px){
+    width: 50%;
+  }
 
   p {
-    font-size: 60px;
+    font-size: 2rem;
     padding: 30px 10px;
     margin-bottom: 20px;
-    text-align: left;
+    text-align: center;
     color: #E8505B;
+    @media screen and (min-width: 800px) {
+      font-size: 3.75rem;
+      text-align: left;
+    }
   }
-
-  .hero__button {
-    background-color: #304c28;
-    padding: 10px 30px;
-    border-radius: 1.5rem;
-    color: white;
-    border: none;
-    font-size: 1.5rem;
-  }
+  
 `;
 
 const Hero = () => {
