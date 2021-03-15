dbc4df437515936ad34cb812fbc26a3a82e6bd2d
Jagoda <76485036+jagagoda@users.noreply.github.com>
2021-03-15 20:48:08 +0100

    Update About.js

--- makramiara/src/components/About.js
+++ makramiara/src/components/About.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import Bucket from '../images/bucket.svg';
 import BarMenu from "./BarMenu";
-import Logo from "../images/logo-white.svg";
+import SubMenuFooter from "./SubMenuFooter";
 
-const SectionAbout = styled.div`
+const SectionAbout = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -13,12 +13,13 @@ const SectionAbout = styled.div`
   height: 100vh;
   background-color: #F4F4F4;
   h2 {
-    margin-top: 150px;
-    margin-bottom: 30px;
-    font-size: 75px;
+    font-size: 2rem;
+    margin-top: 9.7rem;
+    margin-bottom: 1rem;
     color: #E8505B;
-    @media screen and (max-width: 768px) {
-      font-size: 2rem;
+    @media screen and (min-width: 800px) {
+      font-size: 4.7rem;
+      margin-bottom: 1.9rem;
     }
 `;
 
@@ -26,54 +27,35 @@ const AboutContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: flex-end;
-  padding-bottom: 30px;
-  width: 1200px;
-  height: 600px;
-  border-radius: 25px;
+  padding-bottom: 1.9rem;
+  width: 75rem;
+  height: 37.5rem;
+  border-radius: 1.6rem;
   background-color: #ffff;
   position: relative;
   img {
     position: absolute;
     top: 0;
     left: 0;
-    transform: translate(135%, 10%);
-    width: 320px;
+    transform: translate(200%, 2%);
+    width: 15rem;
+    font-size: 4rem;
+    @media screen and (min-width: 800px) {
+      font-size: 4.7rem;
+      width: 20rem;
+      transform: translate(135%, 2%);
+    }
   }
-  
-  }
-
   p {
-    max-width: 50%;
-    font-size: 25px;
+    max-width: 30%;
+    font-size: 1.4rem;
     text-align: center;
     color: #263238;
-    @media screen and (max-width: 768px) {
-      font-size: 1.5rem;
+    @media screen and (min-width: 800x) {
+      font-size: 1.6rem;
+      max-width: 50%;
     }
 `;
-const Footer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: center;
-  background-color: #263238;
-  border-radius: 25px 25px 0 0;
-  min-height: 320px;
-  width: 100%;
-  padding-bottom: 30px;
-  p {
-    color: #ffff;
-    padding: 30px;
-    font-size: 20px;
-    font-family: 'Open Sans', sans-serif;
-    z-index: 10;
-  }
-
-  .logo__icon {
-    width: 10%;
-    padding-bottom: 30px;
-  }
-  `;
 
 const About = () => {
     return (
@@ -88,10 +70,7 @@ const About = () => {
                         garderoby.</p>
                 </AboutContainer>
             </SectionAbout>
-            <Footer>
-                <img className="logo__icon" src={Logo} alt="logo Makramiara"/>
-                <p>Made by Jagoda Frątczak 2021</p>
-            </Footer>
+           <SubMenuFooter/>
 
         </>
     );
